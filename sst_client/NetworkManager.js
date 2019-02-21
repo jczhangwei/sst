db252435f8be9b7f8fa625a436fe3749d458b7fd
wei.a.zhang <wei.a.zhang@dena.com>
2019-02-21 21:35:01 +0800

    fix

---
+++ sst_client/NetworkManager.js
@@ -0,0 +1,79 @@
+let WebSocket = require('ws');
+let util = require('util');
+
+let MsgType = {
+    login: "login",
+    logout: "logout",
+    reconnect: "reconnect",
+    business: "business"
+};
+
+class NetworkManager {
+    get is_connected() {
+        return this._is_connected;
+    }
+
+    set is_connected(value) {
+        this._is_connected = value;
+    }
+
+    constructor() {
+        this._ws = null;
+        this._is_connected = false;
+    }
+
+    connect(url, port) {
+        let ws = this.ws = new WebSocket(util.format("ws://%s:%s", url, port));
+
+        ws.onopen = function(e) {
+            console.log('Connection to server opened');
+            this.is_connected = true;
+            let runner = (function* () {
+                while(true) {
+                    yield setTimeout(function() {
+                        runner.next();
+                    }, 1234);
+                    if(this.is_connected) {
+                        this.sendTestMessage();
+                    } else {
+                        break;
+                    }
+                }
+            })();
+            runner.next();
+        };
+
+        ws.onmessage = function(event) {
+            console.log('\nClient received a message', event.data);
+        };
+
+        ws.onclose = function(e) {
+            console.log('connection closed.');
+            this.is_connected = false;
+        };
+
+    }
+
+    sendTestMessage() {
+        ws.send("Client time: " + new Date());
+    }
+
+    login(suid, passwd) {
+
+    }
+
+    send() {
+
+    }
+
+    get ws() {
+        return this._ws;
+    }
+
+    set ws(value) {
+        this._ws = value;
+    }
+}
+
+
+module.exports = NetworkManager;
--- sst_client/app.js
+++ sst_client/app.js
@@ -1,36 +1,4 @@
-let WebSocket = require('ws');
+let NetworkManager = require("NetworkManager");
 
-let ws = new WebSocket("ws://localhost:8080");
-
-let is_connected = false;
-
-ws.onopen = function(e) {
-    console.log('Connection to server opened');
-    is_connected = true;
-    let runner = (function* () {
-        while(true) {
-            yield setTimeout(function() {
-                runner.next();
-            }, 1234);
-            if(is_connected) {
-                sendMessage();
-            } else {
-                break;
-            }
-        }
-    })();
-    runner.next();
-};
-
-ws.onmessage = function(event) {
-    console.log('\nClient received a message', event.data);
-};
-
-ws.onclose = function(e) {
-    console.log('connection closed.');
-    is_connected = false;
-};
-
-function sendMessage() {
-    ws.send("Client time: " + new Date());
-}
+let network_manager = new NetworkManager();
+network_manager.connect("localhost", "8080");
