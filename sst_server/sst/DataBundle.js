3f5ecc954cd613ccced094270865c499101380d7
wei.a.zhang <wei.a.zhang@dena.com>
2019-03-19 20:30:37 +0800

    fix

--- sst_server/sst/DataBundle.js
+++ sst_server/sst/DataBundle.js
@@ -1,4 +1,8 @@
 class DataBundle {
+    get proxy() {
+        return this._proxy;
+    }
+
     get name() {
         return this._name;
     }
@@ -17,100 +21,62 @@ class DataBundle {
      */
     set data(data) {
         this._data = data;
-        this.preprocessData()
+        this.initDataProxy();
     }
 
     constructor() {
         this._data = null;
+        this._proxy = null;
         this._name = null;
     }
 
     /**
-     * Add watcher to monitoring all modification of the data bundle
-     * data 的内部属性被赋值时，会调用此类中的回调，记录修改，data 本身被重新赋值时，在此类中做另外的处理
+     * Add watcher to monitoring all modification of the data_obj bundle
+     * data_obj 的内部属性被赋值时，会调用此类中的回调，记录修改，data_obj 本身被重新赋值时，在此类中做另外的处理
      */
-    preprocessData(data) {
-        data = data || this.data;
+    initDataProxy() {
+        let data_obj = this.data;
 
-        if(!data === null) {
+        if(!data_obj) {
             return;
         }
 
-        let type = typeof data;
-        switch(type) {
-            case "undefined":           // Undefined
-            case "function":            // function
-            case "boolean":             // Boolean
-            case "number":              // Number
-            case "string":              // String
-            case "symbol":              // Symbol
-                break;
-            case "object":              // 任何其他对象
-                for(let key in data) {
-                    if(data.hasOwnProperty(key)) {
-                        let pro = data[key];
-                        if(pro === null) {
-                            continue;
-                        }
-
-                        let type = typeof data;
-                        switch(type) {
-                            case "undefined":           // Undefined
-                            case "function":            // function
-                            case "boolean":             // Boolean
-                            case "number":              // Number
-                            case "string":              // String
-                            case "symbol":              // Symbol
-                                this.reprocessBaseData(data, key, key);
-                                break;
-                            case "object":              // 任何其他对象
-                                this.processObjectProperty(data, key, key);
-                                break;
-                        }
-
-                    }
-
-                }
-                break;
-
-        }
+        let proxy = this.getProxyForObject(data_obj, "");
+        this.proxy = proxy;
 
     }
 
     // todo 处理 Map 和 Array
-    processObjectProperty(obj, property_name, data_path) {
-        var value = obj[property_name];
+    getProxyForObject(data_obj, data_path, parent) {
+        let handler = this.getObjectHandler(data_obj, data_path);
+        let proxy = new Proxy(data_obj, handler);
 
-        switch(type) {
-            case "undefined":           // Undefined
-            case "function":            // function
-                break;
-            case "boolean":             // Boolean
-            case "number":              // Number
-            case "string":              // String
-            case "symbol":              // Symbol
-                this.reprocessBaseData(obj, property_name, data_path);
-                break;
-            case "object":              // 任何其他对象
-                let value = obj[property_name];
+        for(let key in data_obj) {
+            if(data_obj.hasOwnProperty(key)) {
+                let pro = data_obj[key];
+                if(pro === null) {
+                    continue;
+                }
 
-                let self = this;
-                Object.defineProperty(obj, property_name, {
-                    enumerable: true,
-                    configurable: false,
-                    get: function() {
-                        return value;
-                    },
-                    set: function(v) {
-                        value = v;
-                        self.addObjectAssignment(data_path, value);
-                    },
-                });
+                if(typeof pro === "object") {
+                    let p = this.getProxyForObject(data_obj, `${data_path}/${key}`);
 
-                this.processObjectProperty(obj, key, key);
-                break;
+                }
+
+            }
+
         }
 
+        return proxy;
+
+    }
+
+    getObjectHandler(data_obj, data_path) {
+        return {
+            set: function(target, propKey, receiver) {
+
+            }
+        }
     }
 
     reprocessBaseData(obj, property_name, data_path) {
