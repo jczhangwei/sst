4cf04c5a60fbba4ed1ae03e5b6ec7657ab085445
wei.a.zhang <wei.a.zhang@dena.com>
2020-03-11 19:56:36 +0800

    fix

--- gsst_server/src/RouteManager.js
+++ gsst_server/src/RouteManager.js
@@ -1,7 +1,16 @@
+const assert = require('assert');
+let sst_server = require("../src/ProtoClasses").sst_server;
 
 let RouteTable = {
     
 };
+
+class RouteListener {
+    constructor(callback, caller) {
+        this.callback = callback;
+        this.caller = caller;
+    }
+}
 
 class RouteManager {
     static get instance() {
@@ -12,15 +21,21 @@ class RouteManager {
     }
 
     constructor() {
-        this.init();
-    }
-
-    init() {
-
+        this.route_table = new Map();
     }
 
     dist(route, data) {
         
+    }
+
+    registerRoute(msg_name, callback, caller) {
+        let listeners = this.route_table.get(msg_name);
+        if(!listeners) {
+            listeners = new Set();
+            this.route_table.set(msg_name, listeners);
+        }
+
+        listeners.set(new RouteListener(callback, caller));
     }
     
 };
